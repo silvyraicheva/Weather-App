a02ddbfb92daf29498ac30b0212f4462137eb799
Silvy Raicheva <sylvie.raicheva@gmail.com>
2023-12-28 17:42:02 +0200

    Hourly Forecast added

--- script.js
+++ script.js
@@ -2,9 +2,24 @@
 const API_KEY = "357a8d0f267d897e2e19b8a474c00a5c";
 
 // When the button is clicked it calls the fetchWeather function
-document.querySelector("#search-btn").addEventListener("click", (e) => {
-    fetchWeather(document.querySelector("#city").value);
+const searchBtn = document.querySelector("#search-btn");
+searchBtn.addEventListener("click", (e) => {
+    let city = document.querySelector("#city").value;
+    if (!city) {
+        alert("Please enter a city");
+    } else {
+        fetchWeather(city);
+        e.target.style.cursor = "wait";
+    }
 });
+
+searchBtn.addEventListener("mouseenter", (e) => {
+    e.target.style.cursor = "pointer";
+});
+searchBtn.addEventListener("mouseleave", (e) => {
+    e.target.style.cursor = "default";
+});
+
 // Fetches weather data from the OpenWeatherMap API based on a given city name
 const fetchWeather = (cityName) => {
     // using Current weather data
@@ -16,8 +31,20 @@ const fetchWeather = (cityName) => {
             displayWeather(data);
         })
         .catch((error) => {
-            // console.error(error);
-            console.log("Please type a valid city name", error);
+            alert("Please type a valid city name");
+            console.log(error);
+        });
+    // using 5 day weather forecast
+    const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${API_KEY}`;
+    fetch(forecastURL)
+        .then((result) => result.json())
+        .then((data) => {
+            displayForecast(data.list);
+        })
+        .catch((error) => {
+            // alert("Error fetching forecast");
+
+            console.error(error);
         });
 };
 // Updates the results on the page based on the search
@@ -32,8 +59,10 @@ const displayWeather = (data) => {
     // wind speed
     const { speed } = data.wind;
     console.log(data);
-    console.log(name, icon, description, temp, feels_like, humidity, speed);
+
+    // console.log(name, icon, description, temp, feels_like, humidity, speed);
     // changing the background of the page with a photo from the city that is searched
+
     document.querySelector(
         "body"
     ).style.backgroundImage = `url(https://source.unsplash.com/random/1600x900/?${name})`;
@@ -51,5 +80,31 @@ const displayWeather = (data) => {
     document.querySelector(".humidity").innerHTML = `Humidity: ${humidity}%`;
     document.querySelector(".wind").innerHTML = `Wind: ${speed}km/h`;
 
-    document.querySelector(".weather-card").classList.remove("content");
+    document.querySelector(".weather-card").classList.remove("hidden");
+};
+// Displays the forecast for the next 24 hours
+const displayForecast = (data) => {
+    document.querySelector(".hourly-container").innerHTML = "";
+    // weather in the next 24 hours (3h intervals)
+    let next24 = data.slice(0, 8);
+    console.log(next24);
+    // extracting the hour, temp and icon and creating a new element card to add to the forecast section
+    next24.forEach((item) => {
+        // Convert timestamp to milliseconds
+        const dateTime = new Date(item.dt * 1000);
+        const hour = dateTime.getHours();
+        const date = item.dt_txt.substr(0, 10);
+        const { temp } = item.main;
+        const { icon } = item.weather[0];
+
+        const newItem = `
+        <div class="hourly-card">
+            <span>${date}</span>
+            <span>${hour}:00h</span>
+            <img src="https://openweathermap.org/img/wn/${icon}.png" alt="">
+            <span>${parseInt(temp)}°C</span>
+        </div>`;
+
+        document.querySelector(".hourly-container").innerHTML += newItem;
+    });
 };
